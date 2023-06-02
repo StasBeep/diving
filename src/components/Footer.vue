91f1a8218c50b4442d0c6de92e2197967058fc01
StasBeep <stas-stas.stanislav@mail.ru>
2023-06-02 23:57:44 +0300

    Добавление файлов и элементов footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="footer">
+    <div class="footer-left">
+        <img src="../assets/logo_aquatica.svg" alt="logo">
+        <span>{{ telephoneCompany }}</span>
+        <span>{{ emailCompany }}</span>
+    </div>
+    <div class="footer-right">
+        <MenuRouter />
+        <MenuSocial />
+        <span>Все права защищены © 2010-{{ nowYear }}</span>
+        <span>Обучение дайвингу в Москве</span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import MenuRouter from '../components/MenuRouter.vue'
+import MenuSocial from '../components/MenuSocial.vue'
+
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  name: 'FooterBlock',
+
+  computed: {
+    ...mapState({
+      emailCompany: 'emailCompany',
+      telephoneCompany: 'telephoneCompany'
+    }),
+
+    ...mapGetters({
+      nowYear: 'getNowYear'
+    })
+  },
+
+  components: {
+    MenuRouter,
+    MenuSocial
+  }
+}
+</script>
+
+<style src="../scss/style.scss" lang="scss" scoped>
