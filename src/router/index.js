3a31558d075ce37cd4a88bad96ccf5bc317e4f83
StasBeep <stas-stas.stanislav@mail.ru>
2023-06-02 17:54:37 +0300

    Удаление ненужного + подключение стилей + шрифтов + настройка mix и fonts

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import TemplateView from '../views/TemplateView.vue'
 
 Vue.use(VueRouter)
 
@@ -10,23 +9,16 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
-  },
+  }/*
   {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/semantica',
-    name: 'semantica',
-    // route level code-splitting
-    // this generates a separate chunk (semantica.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: TemplateView
+    // component: () => import('../views/AboutView.vue')
   }
+  */
 ]
 
 const router = new VueRouter({
