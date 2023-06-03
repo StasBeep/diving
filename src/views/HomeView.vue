9f08396463999680da8dfff0a66bdc639676275a
StasBeep <stas-stas.stanislav@mail.ru>
2023-06-03 04:14:11 +0300

    добавление элементов header + стилизация меню наверху

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,26 @@
     <div class="content-without-footer">
       <!--Шапка сайта-->
       <header class="header">
+        <nav class="header-secondmenu">
+          <MenuRouter class="header-secondmenu-route" :colorElement="colorElement" />
+          <ModalFeedback class="header-secondmenu-feedback" />
+          <MenuSocial class="header-secondmenu-social" />
+        </nav>
+        <div class="info">
+          <img src="../assets/logo_aquatica.svg" alt="">
+          <span>Путешествие в тысячу миль</span>
+          <span>Начинается с первого клика</span>
+          <button>Хочу нырять...</button>
+          <div class="info-details">
+            <span>Работаем с 2009 года</span>
+            <span>Более 2500 учеников</span>
+          </div>
+          <ol>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ol>
+        </div>
       </header>
 
       <!--Основная часть-->
@@ -23,12 +43,22 @@
 </template>
 
 <script>
+import MenuRouter from '../components/MenuRouter.vue'
+import MenuSocial from '../components/MenuSocial.vue'
+import ModalFeedback from '../components/ModalFeedback.vue'
 
 export default {
   name: 'HomeView',
   components: {
+    MenuRouter,
+    MenuSocial,
+    ModalFeedback
+  },
 
-  }
+  data: () => ({
+    colorElement: 'white',
+    findingElement: 'footer'
+  })
 }
 </script>
 
