4d7d6d4391ce5458d5e5f5e4806bbfa86ebccc1d
StasBeep <stas-stas.stanislav@mail.ru>
2023-06-05 00:56:23 +0300

    Готовый шаблон под новости блог и предложения, но без API

--- src/components/NewsInfo.vue
+++ src/components/NewsInfo.vue
@@ -6,6 +6,24 @@
         <p class="newscomponent-li-text">Курс мастерской плавучести Peak Perfomance Buoyancy.</p>
         <a href="#" class="newscomponent-li-link" target="_blank">Перейти &#10140;</a>
     </li>
+    <li class="newscomponent-li">
+        <img class="newscomponent-li-img" src="../assets/header-second.jpg" alt="escort" width="318" height="157">
+        <span class="newscomponent-li-title">Peak Perfomance Buoyancy</span>
+        <p class="newscomponent-li-text">Курс мастерской плавучести Peak Perfomance Buoyancy.</p>
+        <a href="#" class="newscomponent-li-link" target="_blank">Перейти &#10140;</a>
+    </li>
+    <li class="newscomponent-li">
+        <img class="newscomponent-li-img" src="../assets/header-second.jpg" alt="escort" width="318" height="157">
+        <span class="newscomponent-li-title">Peak Perfomance Buoyancy</span>
+        <p class="newscomponent-li-text">Курс мастерской плавучести Peak Perfomance Buoyancy.</p>
+        <a href="#" class="newscomponent-li-link" target="_blank">Перейти &#10140;</a>
+    </li>
+    <li class="newscomponent-li">
+        <img class="newscomponent-li-img" src="../assets/header-second.jpg" alt="escort" width="318" height="157">
+        <span class="newscomponent-li-title">Peak Perfomance Buoyancy</span>
+        <p class="newscomponent-li-text">Курс мастерской плавучести Peak Perfomance Buoyancy.</p>
+        <a href="#" class="newscomponent-li-link" target="_blank">Перейти &#10140;</a>
+    </li>
   </ol>
 </template>
 
