ee97f2afd48832e7c2feff6e4d4f2a68979f1698
StasBeep <stas-stas.stanislav@mail.ru>
2023-06-04 23:38:21 +0300

    Добавление элементов для первой карточки

--- src/components/NewsInfo.vue
+++ src/components/NewsInfo.vue
@@ -1,6 +1,11 @@
 <template>
-  <ol>
-    <li>1</li>
+  <ol class="newscomponent">
+    <li class="newscomponent-li">
+        <img class="newscomponent-li-img" src="../assets/header-second.jpg" alt="escort" width="318" height="157">
+        <span class="newscomponent-li-title">Peak Perfomance Buoyancy</span>
+        <p class="newscomponent-li-text">Курс мастерской плавучести Peak Perfomance Buoyancy.</p>
+        <a href="#" class="newscomponent-li-link">Перейти &#10140;</a>
+    </li>
   </ol>
 </template>
 
