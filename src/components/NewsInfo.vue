83e8261a2a0abe3793e467b90b09d3509fb3a3cd
StasBeep <stas-stas.stanislav@mail.ru>
2023-06-05 00:31:55 +0300

    Готовый шаблон под первую карточку

--- src/components/NewsInfo.vue
+++ src/components/NewsInfo.vue
@@ -4,7 +4,7 @@
         <img class="newscomponent-li-img" src="../assets/header-second.jpg" alt="escort" width="318" height="157">
         <span class="newscomponent-li-title">Peak Perfomance Buoyancy</span>
         <p class="newscomponent-li-text">Курс мастерской плавучести Peak Perfomance Buoyancy.</p>
-        <a href="#" class="newscomponent-li-link">Перейти &#10140;</a>
+        <a href="#" class="newscomponent-li-link" target="_blank">Перейти &#10140;</a>
     </li>
   </ol>
 </template>
