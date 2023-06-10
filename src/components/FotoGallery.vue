a1c6f4434ea89a6658b4b8730fd95371ce5269e6
StasBeep <stas-stas.stanislav@mail.ru>
2023-06-10 19:19:18 +0300

    добавление файлов для работы с компонентом фото-галерея

---
+++ src/components/FotoGallery.vue
@@ -0,0 +1,17 @@
+<template>
+  <ol>
+    <li>1</li>
+  </ol>
+</template>
+
+<script>
+export default {
+  name: 'FotoGallery',
+
+  props: {
+    fotoArray: Array
+  }
+}
+</script>
+
+<style src="../scss/style.scss" lang="scss" scoped>
